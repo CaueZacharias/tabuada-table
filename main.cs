afa98f7695019417765ff63c587cf8b76dc529f8
KauanApolinario <85881624+KauanApolinario@users.noreply.github.com>
2021-06-14 11:45:17 -0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,9 +1,7 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
+Nome: Teste
+RA: Teste
 
 *******************************************************************************/
 using System;
